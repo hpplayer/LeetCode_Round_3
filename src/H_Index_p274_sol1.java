9b7aeb04eb0a903569b6e103991e54e42dd935d9
hpPlayer <hpplayer@live.cn>
2016-02-20 23:21:02 -0800

    DayN  + 34

--- src/H_Index_p274_sol1.java
+++ src/H_Index_p274_sol1.java
@@ -1,12 +1,37 @@
+import java.util.Arrays;
+
+/*
+274. H-Index
+
+Given an array of citations (each citation is a non-negative integer) of a researcher, write a function to compute the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: "A scientist has index h if h of his/her N papers have at least h citations each,
+and the other N − h papers have no more than h citations each."
+
+For example, given citations = [3, 0, 6, 1, 5], which means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, his h-index is 3.
+
+Note: If there are several possible values for h, the maximum one is taken as the h-index.
+
+Hint:
+
+An easy approach is to sort the array first.
+What are the possible values of h-index?
+A faster approach is to use extra space.
+*/
+
 /**
  * Naive approach
  * 
  * The definition of H-index can be seen from below graph:
  *  
  * citations(ascending)
- * |../
- * | / ..
+ * |.. /
+ * |. / 
+ * | /  ...  
  * |/_45__ papers (ascending)
+ * 
+ * (we should have less papers having larger citations while more papers having smaller citations)
  * 
  * The H-index is the papers index that most close to the 45-diagonal. It must in the part above diagonal. If it is in below part, then we wouldn't 
  * have enough papers to reach citations with value of papers index
@@ -21,10 +46,42 @@
  * 
  * In extreme case that we couldn't find H-index in the input array, like input [0], we should return 0. As H-index does not exist
  * 
+ * Time complexity: O(NlogN + N)
+ * Space complexity: O(N)
+ * 
+ * Notice:
+ * It is possible that we have H-index as len. Example: [1]. So H-index ranges from 0 to len
+ * So the range of H-index is from 0 -> len. 0 means none num of papers meet the requirement while len means total num of papers meet the requirement
+ * 
+ * sol1 is based on the graph and more widely used due to its convenience but it is slow, as we need to sort first
+ * In sol1, we scan the paper axis backward and find the first index that gives citations[i] >= n - i where n - i is num of books as well as the 
+ * index in paper axis. Because small citation val has large paper num value and vice versa, n - i actually help us get the correct val at paper axis
+ * 
+ * sol2 is based on the definition the problem gives us. It is faster
+ * In sol2, it is more understandable to look the definition only, as we have converted the citation values, and we can no longer use the graph 
+ * to anaylze the solution
  * 
  * @author hpPlayer
  * @date Feb 20, 2016 2:34:03 PM
  */
 public class H_Index_p274_sol1 {
-
+    public int hIndex(int[] citations) {
+        //we firstly sort the array, and put smaller citations in front. Our array will then have those property: for citation c at index i
+        //we will have (len - i) papers that have citations >= c. Therefore when we are reading the array forward, we are actually reading the 
+    	//paper axis backward, because we are reading smaller citations first and they can be accumulated.
+    	//Our task is to find the index i that has c >= (len - i)
+        
+        Arrays.sort(citations);
+        int len = citations.length;
+        
+        for(int i = 0; i < len; i++){
+        	//len - i is the num of books that has citation >= i;
+        	//citations[i] is the val of citations at index i
+            if(citations[i] >= len - i) return len - i;
+        }
+        
+        //if no citations in our book can meet the requirement, then we need to return 0
+        
+        return 0;
+    }
 }
---
+++ src/H_Index_p274_sol2.java
@@ -0,0 +1,53 @@
+/**
+ * Count Sort solution
+ * 
+ * This solution is based on the definition that H-index k means we have k papers have citation k and n - k papers have citations < k
+ * Instead of scanning the citations[], we actually convert array to papers[], each cell's index is the citation value and its val means 
+ * the papers it has. Therefore now we no longer need sort() function to sort the array, and we can get our solution within O(N) time
+ * 
+ * 
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ * 
+ * @author hpPlayer
+ * @date Feb 20, 2016 11:16:40 PM
+ */
+public class H_Index_p274_sol2 {
+    public int hIndex(int[] citations) {
+        //count sort solution. Instead of using built-in sort function, now we build a new array with citation value
+        //as the index, and assign each paper to its corresponding cell.
+        //Citation values can vary, but our H-index will only have range from 0 to len, where 0 means no index meets
+        //requirement while n means the the last index meets requirement, so we will create an array with n + 1 len
+        //to include each possible H-index value. For citation val >= n + 1, we will assign them to last index
+        //indicating index n can include their impacts
+        //Notice: don't use graph to analyze this soltuion as we have modified the citation value based on the 
+        //word definition so the graph will not be same as it should to be 
+        
+        int len = citations.length;
+        
+        //use len + 1 to include 0 to len possible H-indexes(both inclusive)
+        int[] papers = new int[len + 1];
+        
+        //update papers array
+        for(int citation : citations){
+            if(citation >= len +1) papers[len]++;
+            else papers[citation]++;
+        }
+        
+        //scan the array backward so we can accumulate paper numbers to smaller citation val
+        for(int i = len; i >= 0; i--){
+            //accumulate papers when we are visiting last second and ahead cells
+            if(i < len) papers[i] += papers[i+1];
+            
+            //based on the definition, if we found the max possible index that we have k papers with k citations,
+            //then it would be our H-index
+            //now if at index i we have papers[i] papers >= citations i, and it is the max index, then it must 
+            //be the H-index
+            if(papers[i] >= i) return i;
+        }
+        
+        //can't find H-index, return -1
+        //Theoretically, it is not possible
+        throw new IllegalArgumentException("not possible");
+    }
+}
